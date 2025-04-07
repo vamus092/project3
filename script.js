e6b2c61b6fb0b8bbd88b931b9077ff4aa0e40b1e
vamus092 <dsuvam954@gmail.com>
2025-04-08 01:52:45 +0530

    update animation

--- script.js
+++ script.js
@@ -1,16 +1,51 @@
+function homepageAnimation(){
+    gsap.set(".slid",{scale:5 } )
 
+var tl=gsap.timeline({
+    scrollTrigger: {
+        trigger:".home",
+        start:"top top",
+        end:"bottom bottom",
+        markers:true,
+        scrub: 2
+    }
 
+})
 
-// gsap.to(".vdodiv", {
-//     scrollTrigger: {
-//         trigger:".home",
-//         start:"top top",
-//         end:"bottom top",
-//         pin:true,
-//         scrub: 2
-//     },
-// '--clip':"0%",
-// ease:  Power2,
-// duration: 2
+tl
+.to(".vdodiv", {
+'--clip':"0%",
+ease:  Power2,
+} ,'a' )
 
-// })
+.to(".slid",{
+    scale:0.7, 
+    ease: Power2
+} ,'a' )
+
+.to(".rgt",{
+   xPercent:5,
+   stagger: .03,
+    ease: Power4
+} ,'b' )
+.to(".lft",{
+    xPercent:-5,
+   stagger: .03,
+    ease: Power4
+} ,'b' )
+}
+
+gsap.to(".slide",{
+
+    scrollTrigger:{
+        trigger:".real",
+        start: "top top",
+        end:" bottom bottom",
+        markers:true,
+        scrub:2
+    },
+    xPercent:-200,
+    ease:Power4
+})
+
+homepageAnimation();
