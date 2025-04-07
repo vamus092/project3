cead86d00d93de31383fabfd4dee85bd0391b13d
vamus092 <dsuvam954@gmail.com>
2025-04-08 00:20:08 +0530

    clip js

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+
+// gsap.to(".vdodiv", {
+//     scrollTrigger: {
+//         trigger:".home",
+//         start:"top top",
+//         end:"bottom top",
+//         pin:true,
+//         scrub: 2
+//     },
+// '--clip':"0%",
+// ease:  Power2,
+// duration: 2
+
+// })
